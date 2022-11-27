cb91cff45e25d73ec0d93a1c9d9bac7f96ab3cdf
Dani Rosa <danirosa.dev@gmail.com>
2022-11-27 16:52:50 -0300

    test7

--- src/Letras.js
+++ src/Letras.js
@@ -7,8 +7,7 @@ export default function Letras({alfabeto, tentaALetra, clicado}) {
                 <LetterButton data-test="letter"
                     key={a}
                     onClick={() => tentaALetra(a)}
-                    disabled={clicado.includes(a)}
-                    className="letra">
+                    disabled={clicado.includes(a)}>
                     {a}
                 </LetterButton>)}
         </div>
@@ -19,17 +18,13 @@ const LetterButton = styled.button`
     width: 30px;
     height: 30px;
     margin: 5px;
-    background-color: #d4486f;
-    color: #ffffff;
+    background-color: ${props => props.disabled ? "#ef97a3" : "#d4486f"};
+    color: ${props => props.disabled ? "#d4486f" : "#ffffff"};
     border: none;
     border-radius: 5px;
     transition-duration: 0.4s;
     font-size: 20px;
     &:hover{
         filter: brightness(1.2);
-    }
-    &:disabled{
-        background-color: #ef97a3;
-        color: #d4486f;
     }
 `
